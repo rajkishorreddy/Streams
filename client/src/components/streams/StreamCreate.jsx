6de55674db5f47c4cdf973a3cfeb25a1b1b50036
rajkishorreddy <rajakishorbeeravalli@gmail.com>
2021-03-30 23:38:14 +0530

    modified stream create form

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -1,17 +1,48 @@
 import React from "react";
 import { Field, reduxForm } from "redux-form";
 class StreamCreate extends React.Component {
-  renderInput() {
-    return <div>I'm an input</div>;
+  // renderInput(formProps) {
+  //   return <input {...formProps.input} />;
+  // }
+  renderInput({ input, label, meta }) {
+    return (
+      <div className="field">
+        <label>{label}</label>
+        <input {...input} />
+        <div>{meta.error}</div>
+      </div>
+    );
+  }
+  onSubmit(formValues) {
+    // event.preventDefault(); na na not with redux form
+    console.log(formValues);
   }
   render() {
     return (
-      <form>
-        <Field name="title" component={this.renderInput} />
-        <Field name="description" component={this.renderInput} />
+      <form
+        onSubmit={this.props.handleSubmit(this.onSubmit)}
+        className="ui form"
+      >
+        <Field name="title" component={this.renderInput} label="Enter Title" />
+        <Field
+          name="description"
+          component={this.renderInput}
+          label="Enter Description"
+        />
+        <button className="ui button primary">Submit</button>
       </form>
     );
   }
 }
-
-export default reduxForm({ form: "streamCreate" })(StreamCreate);
+const validate = (formValues) => {
+  const errors = {};
+  if (!formValues.title) {
+    errors.title =
+      "what in the world made you think you can create a stream without a title";
+  }
+  if (!formValues.description) {
+    errors.description = "tell people what this stream is about ";
+  }
+  return errors;
+};
+export default reduxForm({ form: "streamCreate", validate })(StreamCreate);
