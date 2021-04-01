acc65d40bcf4b9169a0a242a44b00428a7cd44f7
rajkishorreddy <rajakishorbeeravalli@gmail.com>
2021-04-02 00:26:07 +0530

    updated edit form

--- client/src/components/streams/StreamCreate.jsx
+++ client/src/components/streams/StreamCreate.jsx
@@ -1,61 +1,20 @@
 import React from "react";
-import { Field, reduxForm } from "redux-form";
 import { createStream } from "../../actions";
 import { connect } from "react-redux";
+import StreamForm from "./StreamForm";
+
 class StreamCreate extends React.Component {
-  // renderInput(formProps) {
-  //   return <input {...formProps.input} />;
-  // }
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      );
-    }
-  }
-  renderInput = ({ input, label, meta }) => {
-    const className = `field ${meta.error && meta.touched ? "error" : ""}`;
-    return (
-      <div className={className}>
-        <label>{label}</label>
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-      </div>
-    );
-  };
   onSubmit = (formValues) => {
-    // event.preventDefault(); na na not with redux form
     this.props.createStream(formValues);
   };
   render() {
     return (
-      <form
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-        className="ui form error"
-      >
-        <Field name="title" component={this.renderInput} label="Enter Title" />
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Enter Description"
-        />
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     );
   }
 }
-const validate = (formValues) => {
-  const errors = {};
-  if (!formValues.title) {
-    errors.title =
-      "what in the world made you think you can create a stream without a title";
-  }
-  if (!formValues.description) {
-    errors.description = "tell people what this stream is about ";
-  }
-  return errors;
-};
-const fromWrapped = reduxForm({ form: "streamCreate", validate })(StreamCreate);
-export default connect(null, { createStream })(fromWrapped);
+
+export default connect(null, { createStream })(StreamCreate);
