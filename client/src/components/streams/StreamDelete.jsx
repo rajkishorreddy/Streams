4dea5b1379730ce0df97fc06ee0fd831b1959ce6
rajkishorreddy <rajakishorbeeravalli@gmail.com>
2021-04-02 18:51:54 +0530

    creating model

--- client/src/components/streams/StreamDelete.jsx
+++ client/src/components/streams/StreamDelete.jsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Modal from "../Modal";
 
+//modal rendered as a portal at #modal in html
 const StreamDelete = () => {
-  return <div>StreamDelete</div>;
+  return (
+    <div>
+      StreamDelete
+      <Modal />
+    </div>
+  );
 };
 
 export default StreamDelete;
